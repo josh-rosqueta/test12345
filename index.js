0e563573c6093e02f66b82574f3a201f1571f437
joshua rosqueta <joshua.rosqueta@achievewithoutborders.com>
2023-06-27 14:41:47 +0800

    changes agen

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const cors = require("cors");
-const middlewares = require("middlewares");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const jwt = require("jsonwebtoken");
 const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 require("dotenv").config();
-
-const server = express();
-const viberURL = 'https://chatapi.viber.com/pa/send_message';
 
 const bot = new ViberBot({
 	authToken: process.env.ACCESS_TOKEN,
@@ -17,71 +9,75 @@ const bot = new ViberBot({
 	avatar: "https://developers.viber.com/docs/img/stickers/40122.png" // It is recommended to be 720x720, and no more than 100kb.
 });
 
-async function sendMessageWithButtons(receiverId, text, button1Text, button1Data, button2Text, button2Data) {
-  const message = {
-    receiver: receiverId,
-    type: 'text',
-    text: text,
-    keyboard: {
-      Type: 'keyboard',
-      Buttons: [
-        {
-            Columns: 6,
-            Rows: 1,
-            ActionType: 'reply',
-            ActionBody: button1Data,
-            Text: button1Text,
-            TextSize: 'large'
-          },
-          {
-            Columns: 6,
-            Rows: 1,
-            ActionType: 'reply',
-            ActionBody: button2Data,
-            Text: button2Text,
-            TextSize: 'large'
-          }
-      ]
-    }
-  };
-
-  const options = {
-    uri: viberURL,
-    method: 'POST',
-    headers: {
-      'X-Viber-Auth-Token': process.env.ACCESS_TOKEN,
-      'Content-Type': 'application/json'
-    },
-    body: message,
-    json: true
-  };
-
-  try {
-    const response = await request(options);
-    console.log(response);
-    return message; // Print the response (for debugging)
-  } catch (error) {
-    console.error(error); // Handle the error
-  }
-}
-
-
-function checkUrlAvailability(botResponse, text_received) {
-    let sender_id = botResponse.userProfile.id;
-    
-    const text = 'Choose an option:';
-    const button1Text = 'Start';
-    const button1Data = 'button1';
-    const button2Text = 'End Conversation';
-    const button2Data = 'button2';
-
-
-    sendMessageWithButtons(sender_id, text, button1Text, button1Data, button2Text, button2Data).then(response => {
-    }).catch(error => console.error(error));
-}
-
+const server = express();
 server.use(express.json());
 server.use('/viber/webhook', bot.middleware());
+
+// const viberURL = 'https://chatapi.viber.com/pa/send_message';
+// async function sendMessageWithButtons(receiverId, text, button1Text, button1Data, button2Text, button2Data) {
+//   const message = {
+//     receiver: receiverId,
+//     type: 'text',
+//     text: text,
+//     keyboard: {
+//       Type: 'keyboard',
+//       Buttons: [
+//         {
+//             Columns: 6,
+//             Rows: 1,
+//             ActionType: 'reply',
+//             ActionBody: button1Data,
+//             Text: button1Text,
+//             TextSize: 'large'
+//           },
+//           {
+//             Columns: 6,
+//             Rows: 1,
+//             ActionType: 'reply',
+//             ActionBody: button2Data,
+//             Text: button2Text,
+//             TextSize: 'large'
+//           }
+//       ]
+//     }
+//   };
+
+//   const options = {
+//     uri: viberURL,
+//     method: 'POST',
+//     headers: {
+//       'X-Viber-Auth-Token': process.env.ACCESS_TOKEN,
+//       'Content-Type': 'application/json'
+//     },
+//     body: message,
+//     json: true
+//   };
+
+//   try {
+//     const response = await request(options);
+//     console.log(response);
+//     return message; // Print the response (for debugging)
+//   } catch (error) {
+//     console.error(error); // Handle the error
+//   }
+// }
+
+
+// function checkUrlAvailability(botResponse, text_received) {
+//     let sender_id = botResponse.userProfile.id;
+    
+//     const text = 'Choose an option:';
+//     const button1Text = 'Start';
+//     const button1Data = 'button1';
+//     const button2Text = 'End Conversation';
+//     const button2Data = 'button2';
+
+
+//     sendMessageWithButtons(sender_id, text, button1Text, button1Data, button2Text, button2Data).then(response => {
+//     }).catch(error => console.error(error));
+// }
+
+const PORT = process.env.PORT || 8081;
 
 bot.onSubscribe(response => {
   say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
@@ -105,6 +101,6 @@ bot.onTextMessage(/./, (message, response) => {
 
 bot.getBotProfile().then(response => console.log(`Bot Named: ${response.name}`));
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+server.listen(PORT, async () => {
+  console.log("Server is running on port", PORT);
 });
