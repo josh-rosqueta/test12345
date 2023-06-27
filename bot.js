46425b35d44a174166a59df2eac153006db8db4f
joshua rosqueta <joshua.rosqueta@achievewithoutborders.com>
2023-06-27 17:41:55 +0800

    added auth

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Bot } = require('viber-bot');
 const bot = new Bot({
-  authToken: 'YOUR_AUTH_TOKEN',
+  authToken: '513d6faf5fe7e2c3-86ed98ee495db94b-d885f5b71e8ea0ee',
   name: 'YourBotName',
   avatar: 'https://example.com/avatar.png',
 });
